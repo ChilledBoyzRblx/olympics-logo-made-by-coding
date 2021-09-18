59a7ec4c514f44c518bab3060a1e389c42ef3e9e
ChilledBoyzRblx <86187817+ChilledBoyzRblx@users.noreply.github.com>
2021-09-18 14:33:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+canvas = document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(275, 210, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(375, 210, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(475, 210, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(325, 250, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(425, 250, 40 ,0 , 2 * Math.PI);
+ctx.stroke();
